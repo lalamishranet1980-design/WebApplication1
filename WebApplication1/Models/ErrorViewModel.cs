06b9b2c9e37fcd48a52cb671f091744a3296aac8
lalamishranet1980-design <lalamishra.net1980@gmail.com>
2025-12-16 16:08:57 +0530

    Update ErrorViewModel.cs
    i have made firstchange

--- WebApplication1/Models/ErrorViewModel.cs
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace WebApplication1.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+        public int id {get;set;}
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
