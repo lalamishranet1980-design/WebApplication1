cc8a598fb6d629971700aee8bde7ff440d5ad8b0
lalamishranet1980-design <lalamishra.net1980@gmail.com>
2025-12-16 16:24:02 +0530

    Update ErrorViewModel.cs

--- WebApplication1/Models/ErrorViewModel.cs
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace WebApplication1.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-        public int id {get;set;}
+        public string id {get;set;}
         public string? message {get;set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
